bd486e924d06cce9fd2585a70bff6ae7a2c56647
David wanjohi <davidwagura83@gmail.com>
2024-03-02 11:00:29 +0300

    add error messages in login and register files

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,23 @@
 @section('title', 'login')
 @section('content')
     <div class="container">
+      <div class="mt-5">
+        @if ($errors->any())
+          <div class="col-12">
+            @foreach ($errors->all() as $error)
+              <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+          </div>
+        @endif
+
+        @if (session()->has('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+
+        @if (session()->has('success'))
+        <div class="alert alert-success">{{ session('error') }}</div>
+        @endif
+
         <form class="ms-auto me-auto mt-auto" action="{{ route('login.post') }}" method="POST" style="width: 500px">
           @csrf
             <div class="mb-3">
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -2,22 +2,41 @@
 @section('title', 'registration')
 @section('content')
     <div class="container">
-        <form class="ms-auto me-auto mt-auto" action="{{ route('registration.post') }}" method="POST" style="width: 500px">
-          @csrf
-            <div class="mb-3">
-              <label class="form-label">Fullname</label>
-              <input type="email" class="form-control" name="name">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control" name="email">
-              </div>
+      <div class="mt-5">
+        @if ($errors->any())
+          <div class="col-12">
+            @foreach ($errors->all() as $error)
+              <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+          </div>
+        @endif
+
+        @if (session()->has('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+
+        @if (session()->has('success'))
+        <div class="alert alert-success">{{ session('error') }}</div>
+        @endif
+
+
+      </div>
+      <form class="ms-auto me-auto mt-auto" action="{{ route('registration.post') }}" method="POST" style="width: 500px">
+        @csrf
+          <div class="mb-3">
+            <label class="form-label">Fullname</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email">
+          </div>
   
-            <div class="mb-3">
-              <label  class="form-label">Password</label>
-              <input type="password" class="form-control"  name="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+          <div class="mb-3">
+            <label  class="form-label">Password</label>
+            <input type="password" class="form-control"  name="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
 @endsection
